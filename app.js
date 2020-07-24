dc6b734066cd7a7a015c5e226e2ab7251838c4b7
Gregory Shayko <greg@shaykos.com>
2020-07-23 20:49:08 -0400

    improvements

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var http = require('http');
 
 var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
+    console.log("status checked!");
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World\n");
 });
 
 server.listen(8086);

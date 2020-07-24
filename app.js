03cb9813f23e61ac8b1f1327d456e63ce360f518
Gregory Shayko <greg@shaykos.com>
2020-07-23 20:54:50 -0400

    improvements

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+var io = require('@pm2/io')
 var http = require('http');
 
 var server = http.createServer(function (request, response) {
-    console.log("status checked!");
+
+    var result = pm2.list(function(){
+        // error, return 400
+    })
+    console.log(result);
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World\n");
 });

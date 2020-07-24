bbdd7fc6e742697bfc0c346c5634b15c25362670
Gregory Shayko <greg@shaykos.com>
2020-07-23 20:42:07 -0400

    improvements

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps : [{
+    name: "pm2-web",
+    script: "./app.js",
+    env: {
+      NODE_ENV: "development",
+    },
+    env_production: {
+      NODE_ENV: "production",
+    }
+  }]
+}

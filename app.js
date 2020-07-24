e0ecfabfaa10752b7f4aac2bb8bab2bc55e8d3de
Gregory Shayko <greg@shaykos.com>
2020-07-23 21:11:33 -0400

    improvements

--- app.js
+++ app.js
@@ -3,20 +3,36 @@ var express = require('express')
 const port = 8086
 
 var app = express()
-app.get('/:app', function (req, res) {
-    pm2.connect(function (err) {
 
-        if (err) {
-            console.error(err);
-            process.exit(2);
-        }
-        else {
-            pm2.describe('', (err, list) => {
-                console.log(err, list);
-                res.send(req.params.app);
-            });
-        }
-    });
+var apps = [
+    'bot'
+]
+
+app.get('/', function (req, res) {
+    res.send("Provide an /app to check the status ("+apps.join(',')+")");
+});
+
+app.get('/:app', function (req, res) {
+
+    if(!apps.contains(req.params.app)){
+        res.send("Invalid app, please provide one of: " + apps.join(','));
+    }else{
+
+        pm2.connect(function (err) {
+    
+            if (err) {
+                console.error(err);
+                process.exit(2);
+            }
+            else {
+                pm2.describe('', (err, list) => {
+                    console.log(err, list);
+                    res.send(req.params.app);
+                });
+            }
+        });
+
+    }
 });
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))

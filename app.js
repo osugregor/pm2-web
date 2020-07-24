a452911b2b32cf07dcadf8b5f3f0444c9a43151f
Gregory Shayko <greg@shaykos.com>
2020-07-23 21:21:05 -0400

    improvements

--- app.js
+++ app.js
@@ -32,9 +32,9 @@ app.get('/:app', function (req, res) {
                 return res.status(500).send("An error occured!");
             }
 
-            console.log(result)
+            console.log(result[0])
             
-            return res.send(result['pm2_env'].status);            
+            return res.send(result[0]['pm2_env'].status);            
         });
     });
     
